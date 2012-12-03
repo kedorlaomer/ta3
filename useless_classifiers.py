7ad52681e4f528b06c5aa2260cf977fe08e1f75e
Alexander Adler <alexander.adler0@googlemail.com>
2012-12-03 16:37:11 +0100

    added a strange heuristic that doesn't really work

--- useless_classifiers.py
+++ useless_classifiers.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# vim: encoding=utf-8
+# encoding=utf-8
 
 ################################################################
 ########################## Funktionen ##########################
@@ -20,7 +20,7 @@ def extremely_normalize_char(char):
 
 # wie extremely_normalize_char, nur dass alle Zeichen eines
 # Wortes derartig behandelt werden
-#
+ 
 def extremely_normalize(token):
     return ''.join(map(extremely_normalize_char, token))
 
@@ -36,39 +36,74 @@ def substrings(token):
 
     return rv
 
-def sgn(x):
-    if sgn > 0:
-        return 1
-    elif sgn < 0:
-        return -1
-    else:
-        return 0
-
 def sort_by_value(dictionary):
-    return sorted(dictionary, lambda x, y: sgn(dictionary[x] - dictionary[y]))
+    return sorted(dictionary, lambda x, y: cmp(dictionary[x], dictionary[y]))
 
-def mini_evaluation(klassi, epsilon):
-    true_positives = 0
-    false_negatives = 0
-    total = 0
+# gegeben goldstandard_words (Liste von von Paaren (token,
+# isGene)), wird ein eine „Verteilung“ prob berechnet, welche
+# dem Konstruktor von SomehowClassifier übergeben werden kann
+def data_to_prob(goldstandard_words, stopwords, genes):
 
-    for (token, is_gene) in goldstandard_words:
-        total += 1
-        classification = klassi.classify_token(token) > epsilon
-        if classification and is_gene:
-            true_positives += 1
-        if not classification and not is_gene:
-            false_negatives += 1
+# Hashtabelle, welche Substrings (wie in der Funktion
+# substrings()) auf Paare (x, y) abbildet. x ist die Anzahl der
+# Vorkommen des Substrings in Gennamen, y die Anzahl in
+# nicht-Gennamen.
+    evaluation = defaultdict(lambda: (0, 0))
 
-    precision = true_positives/float(total)
-    recall = true_positives/float(true_positives + false_negatives)
-    f_measure = 2*precision*recall / (precision+recall)
-    return precision, recall, f_measure
+#   for (name, isGene) in goldstandard_words:
+#       for subs in substrings(name):
+#           (x, y) = evaluation[subs]
+
+#           if isGene:
+#               x += 50
+#           else:
+#               y += 50
+
+#           evaluation[subs] = (x, y)
+
+    for token in stopwords:
+        for w in substrings(token):
+            t = evaluation[w]
+            t = (t[0], t[1]+1)
+            evaluation[w] = t
+
+    for token in genes:
+        for w in substrings(token):
+            t = evaluation[w]
+            t = (t[0]+100, t[1])
+            evaluation[w] = t
+
+    EPSILON = 0.0001
+# bildet jeden Substring auf die Wahrscheinlichkeit ab, in einem
+# Gennamen vorzukommen bzw. nicht in einem Gennamen
+# vorzukommen); EPSILON wird genommen, falls keine
+# Wahrscheinlichkeit bestimmt werden kann
+    yesProbability = defaultdict(lambda: EPSILON)
+    noProbability = defaultdict(lambda: EPSILON)
+
+# über wieviele Substrings (nicht notwendigerweise verschieden)
+# läuft die Statistik?
+    total = float(sum([x+y for (x, y) in evaluation.values()]))
+
+# prob bildet Substrings auf irgendwas ab, was mit deren
+# Vorkommen in Gennamen zu tun hat
+    prob = defaultdict(lambda: EPSILON)
+
+    for substring in evaluation.keys():
+        (yes, no) = evaluation[substring]
+        yesProbability[substring] = yes
+        noProbability[substring] = no
+        yes = float(max(EPSILON, yes))
+        no = float(max(EPSILON, no))
+        prob[substring] = (yes/(yes+no))
+
+    return prob
 
 ################################################################
 ######################## Klassifizierer ########################
 ################################################################
 
+# unsinniger Classifier
 class DifferenceClassifier:
     _difference = None
 
@@ -101,6 +136,7 @@ class DictionaryClassifier:
             else:
                 return 0.01
 
+# berechnet die Summe verschiedener Classifier
 class OrClassifier:
     _classifiers = None
 
@@ -110,6 +146,18 @@ class OrClassifier:
     def classify_token(self, token):
         return sum([classifier.classify_token(token) for classifier in self._classifiers])
 
+# berechnet die Summe der Gewichte aller Substrings, die in prob
+# vorkommen
+class SomehowClassifier:
+    _prob = None
+
+    def __init__(self, prob):
+        self._prob = prob
+
+    def classify_token(self, token):
+        # Faktor 2.18 durch Experimente gefunden
+        l = max(len(token)-2, 1)**2*2.18
+        return sum([self._prob[w] for w in substrings(token)])/l
 
 ################################################################
 ########################### Programm ###########################
@@ -123,18 +171,18 @@ from pylab import *
 stopwords = set()
 
 for token in open("english_stop_words.txt"):
-    stopwords.add(token.strip().lower())
+    stopwords.add(extremely_normalize(token.strip().lower()))
 
 # lies human-genenames.txt ein
 given_genes = set()
 
 for token in open("human-genenames.txt"):
-    given_genes.add(token.strip().lower())
+    given_genes.add(extremely_normalize(token.strip()))
 
 # lies goldstandard.iob ein
 
 # Format von goldstandard_words: Liste von Paaren (token,
-# is_gene), wobei token ein String (ein Token) und is_gene ein
+# isGene), wobei token ein String (ein Token) und isGene ein
 # Boole'scher Wert ist, welcher angibt, ob token laut
 # goldstandard.iob ein Gen ist.
 
@@ -143,44 +191,21 @@ for line in open("goldstandard.iob"):
     content = line.split("\t")
     if len(content) == 2:
         left, right = content
-        if True: #left not in stopwords:
+        if rand() < 0.5: # nur 50% betrachten
             right = right.strip() != "O"
             goldstandard_words.append((extremely_normalize(left), right))
 
-# diese FreqDists enthalten die von Gene bzw. nicht-Gene
-# typischen Substrings
-gene_substrings = FreqDist()
-notgene_substrings = FreqDist()
+# konstruiere daraus Klassifizierer
+klassi = SomehowClassifier(data_to_prob(goldstandard_words, stopwords, given_genes))
 
-for (token, is_gene) in goldstandard_words:
-    dist = gene_substrings if is_gene else notgene_substrings
-    dist.update(substrings(token))
-
-difference = defaultdict(float)
-size1 = float(gene_substrings.N())
-size2 = float(notgene_substrings.N())
-
-# difference ist eine Hashtabelle, wobei die Schlüssel von
-# Substrings und die Werte Differenzen in ihrer Häufigkeit sind.
-# Positiv bedeutet: häufig in Gennamen; negativ bedeutet: selten
-# in Gennamen.
-
-for substr in (gene_substrings + notgene_substrings).keys():
-    v1 = gene_substrings.get(substr) or 0
-    v2 = notgene_substrings.get(substr) or 0
-
-    difference[substr] = v1/size1 - v2/size2
-
-r = xrange(-10, 10+1)
-A = array([array(r), zeros_like(r)])
-A.dtype = dtype('float64')
-
-for (i, e) in enumerate(xrange(-10, 10+1)):
-    if e != 0:
-        epsilon = 0.2/e
-        print "epsilon = ", epsilon
-        klassi = OrClassifier([DictionaryClassifier(given_genes, stopwords),
-                              DifferenceClassifier(difference)])
-        precision, recall, f_measure = mini_evaluation(klassi, epsilon)
-        A[0, i] = epsilon
-        A[1, i] = f_measure
+# erzeuge goldstandard.predict
+with open("goldstandard.iob") as inp:
+    with open("goldstandard.predict", "w") as out:
+        for line in inp.xreadlines():
+            content = line.strip().split("\t")
+            if len(content) == 2:
+                (token, _) = content
+                token = extremely_normalize(token)
+                result = klassi.classify_token(token) > 0.5
+                result = "B-protein" if result else "O"
+                out.write("%s\t%s\n" % (token, result))
